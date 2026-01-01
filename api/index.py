b0e72f51d1bda918a2a4f81cc35f76025b4d413c
Yash Mane <tank@localhost.localdomain>
2026-01-01 15:13:12 +0530

    Add vercel_wsgi for Flask deployment

--- api/index.py
+++ api/index.py
@@ -8,6 +8,8 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app  # your Flask app
 from vercel_wsgi import handle_request
 
-def handler(environ, start_response):
-    # Wrap the Flask app for Vercel serverless
-    return handle_request(app, environ, start_response)
+#def handler(environ, start_response):
+#    # Wrap the Flask app for Vercel serverless
+#   return handle_request(app, environ, start_response)
+
+handler = handle_request(app)
