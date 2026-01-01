9d8c39b1d632462ce5b8f83b96f39afa4a08d903
Yash Mane <tank@localhost.localdomain>
2026-01-01 15:23:53 +0530

    fix requirements for Vercel

--- api/index.py
+++ api/index.py
@@ -6,10 +6,13 @@ import psycopg2
 # Add project root so we can import app if it's in another file
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app  # your Flask app
-from vercel_wsgi import handle_request
+#from vercel_wsgi import handle_request
 
 #def handler(environ, start_response):
 #    # Wrap the Flask app for Vercel serverless
 #   return handle_request(app, environ, start_response)
 
-handler = handle_request(app)
+def handler(environ, start_response):
+    return app(environ, start_response)
+
+#handler = handle_request(app)
